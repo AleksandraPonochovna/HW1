394644f88c3d7b22e43e0ae27704404422f495b8
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-29 19:59:28 +0300

    Update FootballMatch.java

--- FootballMatch.java
+++ FootballMatch.java
@@ -2,37 +2,25 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class MatchFootball {
 
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        Main main = new Main();
-
+        MatchFootball start = new MatchFootball();
         int randomFirstNumber = (int) (Math.random() * 10);
         int randomSecondNumber = (int) (Math.random() * 10);
-
         System.out.println("Guess result first team: ");
         int userFirstNumber = input.nextInt();
         System.out.println("Guess result second team: ");
         int userSecondNumber = input.nextInt();
         System.out.println("Result first team: " + randomFirstNumber);
         System.out.println("Result second team: " + randomSecondNumber);
-
-        main.resultMatch(randomFirstNumber, randomSecondNumber, userFirstNumber, userSecondNumber);
-
+        System.out.println("Result is: " + start.resultMatch(randomFirstNumber, randomSecondNumber, userFirstNumber, userSecondNumber));
     }
 
     public int resultMatch(int randomFirstNumber, int randomSecondNumber, int userFirstNumber, int userSecondNumber) {
-
-        int result;
-
-        result = (randomFirstNumber == userFirstNumber && randomSecondNumber == userSecondNumber) ? 2 :
+        return (randomFirstNumber == userFirstNumber && randomSecondNumber == userSecondNumber) ? 2 :
                 (randomFirstNumber > randomSecondNumber && userFirstNumber > userSecondNumber) ||
                         (randomFirstNumber < randomSecondNumber && userFirstNumber < userSecondNumber) ? 1 : 0;
-
-        System.out.println("Result of match: " + result);
-        return result;
     }
 }
-
