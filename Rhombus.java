dd50c82bc1f048de1b20247dc90b60a7af39e901
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-05-29 20:13:16 +0300

    Update Rhombus.java

--- Rhombus.java
+++ Rhombus.java
@@ -1,1 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Rhombus {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter maximum number of asterisks: ");
+        int maxCountAsterisk = input.nextInt();
+        drawRhombus(maxCountAsterisk);
+    }
+
+
+    public static void drawRhombus(int maxCountAsterisk) {
+        int numOfRowsAndColumns = maxCountAsterisk * 2 - 1;
+        int countOfAsterisk = 0;
+        char space = ' ';
+        char[][] array = new char[numOfRowsAndColumns][numOfRowsAndColumns];
+        for (int i = 0; i < numOfRowsAndColumns; i++) {
+            System.out.println(" ");
+            if (i >= maxCountAsterisk) {
+                countOfAsterisk = countOfAsterisk - 1;
+            } else {
+                countOfAsterisk = i + 1;
+            }
+            for (int j = 0; j < numOfRowsAndColumns; j++) {
+                array[i][j] = space;
+                if (j == maxCountAsterisk - countOfAsterisk) {
+                    int localCountOfAsterisk = countOfAsterisk;
+                    while (localCountOfAsterisk > 0) {
+                        System.out.print(" ");
+                        array[i][j] = '*';
+                        System.out.print(array[i][j]);
+                        localCountOfAsterisk--;
+                    }
+                } else {
+                    System.out.print(array[i][j]);
+                }
+            }
+        }
+        System.out.println(" ");
+    }
+}
 
